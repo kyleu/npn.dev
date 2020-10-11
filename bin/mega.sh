c0134d6a0c28bb24d1fbac0c1186c4dd426cd87c
Kyle Unverferth <kyle@kyleu.com>
2020-10-11 13:39:53 -0600

    v0.0.16

--- bin/mega.sh
+++ bin/mega.sh
@@ -6,7 +6,14 @@ set -euo pipefail
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $dir/..
 
+bin/tag.sh $1
+git push
+
 rm -rf build
 bin/build-all.sh
 bin/package.sh
+
+gh release create "v$1" -n "v$1"
+gh release upload "v$1" build/package/*
+
 bin/deploy.sh
--- npndatabase/go.mod
+++ npndatabase/go.mod
@@ -6,7 +6,7 @@ require (
 	emperror.dev/errors v0.7.0
 	github.com/jackc/pgx/v4 v4.7.2
 	github.com/jmoiron/sqlx v1.2.0
-	github.com/kyleu/npn/npncore v0.0.15 // npn
+	github.com/kyleu/npn/npncore v0.0.16 // npn
 	logur.dev/logur v0.16.2
 )
 
