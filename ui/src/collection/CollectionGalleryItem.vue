f5f06648816cfa7b3a6e73a2c053ba2712b20cdb
Kyle Unverferth <kyle@kyleu.com>
2020-11-08 13:51:15 -0700

    Sidebar fix

--- ui/src/collection/CollectionGalleryItem.vue
+++ ui/src/collection/CollectionGalleryItem.vue
@@ -1,12 +1,15 @@
 <template>
   <li>
+    <div class="left">
+      <router-link :class="'collection-link ' + profile.settings.linkColor + '-fg'" :to="'/c/' + cc.coll.key" :title="cc.count + ' requests'">
+        {{ label }}
+      </router-link>
+    </div>
     <div class="right">
       <em v-if="cc.coll.description && cc.coll.description.length > 0" class="ml mr">{{ cc.coll.description }}</em>
       <span class="uk-badge" :title="cc.count + ' requests'">{{ cc.count }}</span>
     </div>
-    <router-link :class="'collection-link ' + profile.settings.linkColor + '-fg'" :to="'/c/' + cc.coll.key" :title="cc.count + ' requests'">
-      {{ label }}
-    </router-link>
+    <div class="clear"></div>
   </li>
 </template>
 
